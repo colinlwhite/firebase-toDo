87b18d682444c0cf1c168a2af8bae06e98fd5622
Colin White <clwhite814@gmail.com>
2018-11-27 19:49:15 -0600

    data is being added to firebase, but not the DOM yet

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import firebase from 'firebase/app';
 import 'bootstrap';
+import $ from 'jquery';
 import apiKeys from '../db/apiKeys.json';
 import './index.scss';
 import createNavbar from './components/Navbar/navbar';
 import loginButton from './components/Auth/auth';
 import authHelpers from './helpers/authHelpers'; // basically importing all the 2 functions in that file
 import tasksPage from './components/tasksPage/tasksPage';
+import buildNewTaskForm from './components/AddEditTasks/addEditTasks';
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
   loginButton();
   authHelpers.checkLoginStatus(tasksPage);
+  $('#show-task-form').on('click', buildNewTaskForm);
 };
 
 initializeApp();
