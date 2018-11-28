78627320e3a26c690f2cae7d8e2a193ba3b3ec3d
Colin White <clwhite814@gmail.com>
2018-11-27 21:23:34 -0600

    wip

--- src/components/AddEditTasks/addEditTasks.js
+++ src/components/AddEditTasks/addEditTasks.js
@@ -3,15 +3,15 @@ import tasksData from '../../helpers/Data/tasksData';
 import initializeTasksPage from '../tasksPage/tasksPage';
 import authHelpers from '../../helpers/authHelpers';
 
-const formBuilder = () => {
+const formBuilder = (theTask) => {
   const form = `
     <div class="form-group">
         <label for="task-title">Task:</label>
-        <input type="text" class="form-control" id="task-title" placeholder="Add a New Task">
+        <input type="text" class="form-control" value="${theTask.task}" id="task-title" placeholder="Add a New Task">
   </div>
   <div class="form-group">
         <label for="complete">Completed</label>
-        <input type="checkbox" class="form-control" id="complete">
+        <input type="checkbox" class="form-control" value="${theTask.isCompleted}" id="complete">
   </div> 
   `;
   return form;
@@ -27,9 +27,12 @@ const gettingTaskFromForm = () => {
 };
 
 const buildNewTaskForm = () => {
+  const emptyTask = {
+    task: '',
+  };
   let domString = '<h1>Add New Task</h1>';
-  domString += formBuilder();
-  domString += '<button id="add-task">Save New Task</button>';
+  domString += formBuilder(emptyTask);
+  domString += '<button id="add-task" class="btn btn-primary">Save New Task</button>';
   $('#create-new-task').html(domString).show();
   $('#tasks').hide();
 };
